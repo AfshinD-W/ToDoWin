e492e62481c81d7e3fb011631ee9259409f70b53
AFSHiN JAFARi <afshinaa705@gmail.com>
2025-04-29 16:28:38 +0330

    add save and load from external file

--- ToDoWin/Form1.cs
+++ ToDoWin/Form1.cs
@@ -1,13 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Windows.Forms;
 
 namespace ToDoWin
 {
     public partial class Form1 : Form
     {
+        string filePath = "tasks.txt";
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadTasks();
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -16,6 +24,7 @@ namespace ToDoWin
             {
                 lstTasks.Items.Add(txtTask.Text);
                 txtTask.Clear();
+                SaveTasks();
             }
             else
             {
@@ -28,11 +37,31 @@ namespace ToDoWin
             if (lstTasks.SelectedItem != null)
             {
                 lstTasks.Items.Remove(lstTasks.SelectedItem);
+                SaveTasks();
             }
             else
             {
                 MessageBox.Show("لطفاً یک تسک برای حذف انتخاب کنید.", "خطا", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
+        }
+
+        private void LoadTasks()
+        {
+            if (File.Exists(filePath))
+            {
+                string[] tasks = File.ReadAllLines(filePath);
+                lstTasks.Items.AddRange(tasks);
+            }
+        }
+
+        private void SaveTasks()
+        {
+            List<string> tasks = new List<string>();
+            foreach (var item in lstTasks.Items)
+            {
+                tasks.Add(item.ToString());
+            }
+            File.WriteAllLines(filePath, tasks);
         }
     }
 }
